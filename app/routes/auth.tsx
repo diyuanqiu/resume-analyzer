0eab323f8633aedd89f39ae9e71eb851f29c223e
li-yijunDiyuan <1154713680@qq.com>
2026-04-15 19:09:22 +0800

    authenticate

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
     {title: 'Resumind | Auth'},
@@ -8,6 +9,13 @@ export const meta = () => ([
 
 const Auth = () => {
     const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+      if (auth.isAuthenticated) navigate(next);
+    },[auth.isAuthenticated,next])
 
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
